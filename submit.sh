92dbed117c17e6f125f2da06a19173677e6f4ce9
Kai <chengkai.yang@getsafari.ai>
2025-12-12 13:00:18 -0800

    submit.sh -t v2

--- submit.sh
+++ submit.sh
@@ -28,13 +28,13 @@ spark_start=$(date +%s)
   --conf spark.task.maxFailures=3 \
   --conf spark.stage.maxConsecutiveAttempts=3 \
   --executor-cores 2 \
-  --num-executors 8 \
+  --num-executors 6 \
   --verbose \
   /home/hadoop/SparkBWA/SparkBWATest/compliedJarFile/SparkBWA-jdk11-spark357-v10.jar \
   -t /home/hadoop/spark_bwa_tmp \
   -m -r -p \
   --index /home/hadoop/bwa_input_files/chr22.fa \
-  -n 4 \
+  -n 8 \
   -w "-v 3 -t 8 -R @RG\tID:foo\tLB:bar\tPL:illumina\tPU:illumina\tSM:ERR000589" \
   /user/hadoop/ERR000589_1.filt.fastq \
   /user/hadoop/ERR000589_2.filt.fastq \
@@ -55,7 +55,7 @@ echo | tee -a "$LOG"
 echo "[2/2] Running BWA MEM locally..." | tee -a "$LOG"
 bwa_start=$(date +%s)
 
-/home/hadoop/bwa-0.7.19/bwa mem \
+/home/hadoop/bwa-0.7.19/bwa mem -v 3 -t 8 \
   -R "@RG\tID:foo\tLB:bar\tPL:illumina\tPU:illumina\tSM:ERR000589" \
   /home/hadoop/bwa_input_files/chr22.fa \
   /home/hadoop/bwa_input_files/ERR000589_1.filt.fastq \
